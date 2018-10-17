edff8c04ab78946d5da4bfc1cf8a1cd429b7f8c6
Tiffany D <txd334@miami.edu>
2018-10-17 13:33:51 -0400

    Code
    Code Fix

--- Midterm /sketch.js
+++ Midterm /sketch.js
@@ -5,6 +5,7 @@ var angle= 0;
 var x= 450;
 var button;
 var count = 0;
+var nomore;
 
 function preload() {
   gou = loadImage('gou.JPG');
@@ -17,21 +18,21 @@ function setup() {
   textSize(50);
   textAlign(RIGHT);
   fill(191,72,0);
-}
 
-{
+
 button = createButton('Score a Touchdown');
-button.position(800,110);
+button.position(800,140);
 button.mousePressed(increaseCount);
 
-button = createButton('Turnover Chain');
-button.position (800,130);
+button = createButton('Sucks To Be FSwho');
+button.position (800,180);
 button.mousePressed(decreaseCount);
 }
 
 function increaseCount() {
 count += 7;
 }
+
 function decreaseCount() {
   count -= 7;
 }
@@ -47,21 +48,22 @@ function draw() {
   angle += 5;
   pop();
 
+
   if(keyIsPressed){
     if(key == "d") {
       textSize(50);
       text('GO CANES', 300, 720);
       textSize(20);
-      text('Designed By: ', 300,750);
+      text('WOSH WOSH', 300,750);
       textSize(20);
-      text(' Tiffany Demps', 300, 770);
+      text('TDEMPSDESIGNS', 300, 770);
     } else {
       textSize(50)
       text("THE U INVENTED SWAGGER!", 725, 40);
     }
   }
 
-{
+
   text(count, 800, 120);
-}
+
   }
